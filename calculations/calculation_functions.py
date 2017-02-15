05afe463c25b3e7e3d70524c275a92f664137769
Karol <karolramza@gmail.com>
2017-02-15 18:06:15 +0100

    calculator final 1.0

--- calculations/calculation_functions.py
+++ calculations/calculation_functions.py
@@ -1,2 +1,6 @@
 def carbonation(vol, temperature, volume):
-    return 2*vol+temperature/volume
+    sugar = 15.195 * volume / 3.78541178 * (vol - dissolved(temperature))
+    return round(sugar, 2)
+
+def dissolved(temperature):
+    return 3.0378 - (0.050062 * (temperature*9/5+32)) + (0.00026555 * (temperature*9/5+32)**2)
